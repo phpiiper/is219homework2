753aa26b2d238107eb3c9968eae18115df617962
phpiiper <plp@njit.edu>
2024-02-14 21:37:37 -0500

    Level 1 Completed

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,20 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import add, subtract, multiply, divide
 
 def test_addition():
     '''Test that addition function works '''    
     assert add(2,2) == 4
 
 def test_subtraction():
-    '''Test that addition function works '''    
+    '''Test that subtraction function works '''    
     assert subtract(2,2) == 0
+
+def test_multiplication():
+    '''Test that multiplication function works '''    
+    assert multiply(2,2) == 4
+
+def test_subtraction():
+    '''Test that division function works '''    
+    assert divide(2,2) == 1
+    
     
