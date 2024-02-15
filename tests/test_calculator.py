7f0672dd19f0b6218636c7446e90d32a32af63ef
phpiiper <plp@njit.edu>
2024-02-14 21:12:06 -0500

    Level 2 Completed

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,18 @@
-'''My Calculator Test'''
-from calculator import add, subtract
+'''My Calculator Test: Calculator'''
+from calculator import Calculator
 
 def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
+    '''Test that add function works '''    
+    assert Calculator.add(2,2) == 4
 
 def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
-    
+    '''Test that subtract function works '''    
+    assert Calculator.subtract(2,2) == 0
+
+def test_multiply():
+    '''Test that multiply function works '''    
+    assert Calculator.multiply(2,2) == 4
+
+def test_divide():
+    '''Test that divide function works '''    
+    assert Calculator.divide(2,2) == 1
