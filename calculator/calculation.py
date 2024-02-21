de7a597078d6e4836dae23a10787881a53402a9d
phpiiper <plp@njit.edu>
2024-02-21 14:14:48 -0500

    Pylint at 93%

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -11,8 +11,3 @@ class Calculation:
     @staticmethod
     def perform(self):
         return self.operation(self.a,self.b)
-    def __repr__(self):
-        """Return a simplified string representation of the calculation."""
-        # This method makes it easier to understand what the Calculation object represents when printed or logged
-        # The operation.__name__ attribute is used to get the function's name for a more readable output
-        return f"Calculation({self.a}, {self.b}, {self.operation.__name__})"
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-# conftest.py
+''' conftest.py '''
 import pytest
 from decimal import Decimal
 from faker import Faker
@@ -20,10 +20,10 @@ def generate_test_data(num_records):
         b = Decimal(fake.random_number(digits=2)) if _ % 4 != 3 else Decimal(fake.random_number(digits=1))
         operation_name = fake.random_element(elements=list(operation_mappings.keys()))
         operation_func = operation_mappings[operation_name]
-        # Ensure b is not zero for divide operation to prevent division by zero in expected calculation
+        # Ensure b is not zero for divide operation 
+        # to prevent division by zero in expected calculation
         if operation_func == divide:
             b = Decimal('1') if b == Decimal('0') else b
-        
         try:
             if operation_func == divide and b == Decimal('0'):
                 expected = "ZeroDivisionError"
@@ -31,13 +31,10 @@ def generate_test_data(num_records):
                 expected = operation_func(a, b)
         except ZeroDivisionError:
             expected = "ZeroDivisionError"
-        
         yield a, b, operation_name, operation_func, expected
-
 def pytest_addoption(parser):
     '''Add pytest command option to configure amount of pytests to generate'''
     parser.addoption("--num_records", action="store", default=5, type=int, help="Number of test records to generate")
-
 def pytest_generate_tests(metafunc):
     '''Generate Tests for pytest'''
     # Check if the test is expecting any of the dynamically generated fixtures
